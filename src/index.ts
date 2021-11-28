a5fcf171b45d7def5f23a1454687b859519d6d4d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-28 23:27:06 +0800

    refactor into app module object

--- src/index.ts
+++ src/index.ts
@@ -1,76 +1,121 @@
 import './styles.css';
-const API_BREEDS_URL = `https://dog.ceo/api/breeds/list/all`;
-const API_BY_BREED_URL = (breedname) => `https://dog.ceo/api/breed/${breedname}/images`;
-const template = document.querySelector('template');
-const appContainer = document.querySelector('#app');
-const sentinel = document.getElementById('sentinel');
-const observerAlertSection = document.querySelector('.observer');
-let breedsList = [];
 
-const apiFetch = async (url) => {
-  const res = await fetch(url);
-  if (res.ok) {
-    const json = await res.json();
-    return json;
-  } else {
-    return [];
-  }
-};
+const app = {
+  API_BREEDS_URL: `https://dog.ceo/api/breeds/list/all`,
+  API_BY_BREED_URL: (breedname) => `https://dog.ceo/api/breed/${breedname}/images`,
+  template: document.querySelector('template'),
+  appContainer: document.querySelector('#app'),
+  sentinelElement: document.getElementById('sentinel'),
+  observerAlertSection: document.querySelector('.observer'),
+  breedsList: [],
 
-const fetchBreedImg = async (breed) => {
-  const breedParam = API_BY_BREED_URL(breed);
-  const { message = [] } = await apiFetch(breedParam);
-  const [firstImg = ''] = message;
-  return firstImg;
-};
-
-async function createBreedElement(breed: string) {
-  const image = await fetchBreedImg(breed);
-  const clone = template.content.firstElementChild.cloneNode(true) as any;
-  const [img, h2, p] = <[HTMLImageElement, HTMLHeadingElement, HTMLParagraphElement]>(
-    clone.querySelectorAll('img, h2, p')
-  );
-  h2.textContent = breed.toUpperCase();
-  p.textContent = `What a lovely ${breed}!`;
-  img.src = image;
-  img.alt = breed;
-
-  appContainer.appendChild(clone);
-}
-
-const loadBreedsList = (async () => {
-  const breedsList = await apiFetch(API_BREEDS_URL);
-  return Object.keys(breedsList.message);
-})();
-
-const createFirstFiveSections = async (breedsList) => {
-  let i = 0;
-  while (i < 5 && breedsList.length > 0) {
-    i++;
-    const breed = breedsList.pop();
-    await createBreedElement(breed);
-  }
-  return;
-};
-
-(async () => {
-  breedsList = await loadBreedsList;
-  breedsList.reverse();
-  await createFirstFiveSections(breedsList);
-  const obsEntries = (entries) => {
-    if (entries.length > 0) {
-      const [target] = entries;
-      if (target.isIntersecting && breedsList.length > 0) {
-        observerAlertSection.classList.add('on');
-        observerAlertSection.firstElementChild.textContent = 'Observing';
-        createFirstFiveSections(breedsList);
+  // fetch for reuse
+  apiFetch: async (url: string) => {
+    try {
+      const res = await fetch(url);
+      if (res.ok) {
+        const json = await res.json();
+        return json;
       } else {
-        observerAlertSection.classList.remove('on');
-        observerAlertSection.firstElementChild.textContent = 'Observer Off';
+        return [];
       }
+    } catch (error) {
+      console.error(error);
+      return [];
     }
-  };
+  },
 
-  let observer = new IntersectionObserver(obsEntries);
-  observer.observe(sentinel);
-})();
+  // fetch and return the first breed image
+  fetchBreedImg: async (breed: string) => {
+    const breedParam = app.API_BY_BREED_URL(breed);
+    const { message = [] } = await app.apiFetch(breedParam);
+    const [firstImg = ''] = message;
+    return firstImg;
+  },
+
+  // fetch & return array of breed images
+  fetchAllBreedImages: async (breed: string) => {
+    const breedParam = app.API_BY_BREED_URL(breed);
+    const { message = [] } = await app.apiFetch(breedParam);
+    return message;
+  },
+
+  createBreedElement: async (breed: string) => {
+    const image = await app.fetchBreedImg(breed);
+    const clone = app.template.content.firstElementChild.cloneNode(true) as any;
+    const [img, h2, p, button] = <[HTMLImageElement, HTMLHeadingElement, HTMLParagraphElement, HTMLButtonElement]>(
+      clone.querySelectorAll('img, h2, p, button')
+    );
+    button.dataset.breedName = breed;
+    h2.textContent = breed.toUpperCase();
+    p.textContent = `What a lovely ${breed}!`;
+    img.src = image;
+    img.alt = breed;
+
+    app.appContainer.appendChild(clone);
+  },
+
+  loadBreedsList: async () => {
+    const breedsList = await app.apiFetch(app.API_BREEDS_URL);
+    return Object.keys(breedsList.message);
+  },
+
+  createFirstFiveSections: async (breedsList) => {
+    let i = 0;
+    while (i < 5 && breedsList.length > 0) {
+      i++;
+      const breed = breedsList.pop();
+      await app.createBreedElement(breed);
+    }
+    return;
+  },
+
+  getBreedNameFromClick: (target: EventTarget) => {
+    let breedNameData = '';
+    if (target instanceof HTMLButtonElement) {
+      breedNameData = target.dataset.breedName;
+    } else if (target instanceof SVGElement) {
+      const parentButton = target.closest('button');
+      breedNameData = parentButton.dataset.breedName;
+    }
+    return breedNameData;
+  },
+
+  // event delegate button clicks for each card;
+  addButtonListener: () =>
+    app.appContainer.addEventListener('click', (event) => {
+      const { target } = event;
+      const breedName = app.getBreedNameFromClick(target);
+      console.log(breedName);
+      if (breedName !== '') {
+        localStorage.setItem('breed-name', breedName);
+        console.log(localStorage);
+      }
+    }),
+
+  // init, load breeds and add obeserver for infinite scrolling with doggos!
+  onPageLoad: async () => {
+    app.breedsList = await app.loadBreedsList();
+    app.breedsList.reverse();
+    await app.createFirstFiveSections(app.breedsList);
+    const obsEntries = (entries) => {
+      if (entries.length > 0) {
+        const [target] = entries;
+        if (target.isIntersecting && app.breedsList.length > 0) {
+          app.observerAlertSection.classList.add('on');
+          app.observerAlertSection.firstElementChild.textContent = 'Observing';
+          app.createFirstFiveSections(app.breedsList);
+        } else {
+          app.observerAlertSection.classList.remove('on');
+          app.observerAlertSection.firstElementChild.textContent = 'Observer Off';
+        }
+      }
+    };
+
+    let observer = new IntersectionObserver(obsEntries);
+    observer.observe(app.sentinelElement);
+  },
+};
+window.addEventListener('DOMContentLoaded', app.onPageLoad, { once: true });
+
+// console.log(localStorage.getItem('breed-name'));
